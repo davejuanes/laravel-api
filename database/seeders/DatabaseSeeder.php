8011ff905f6f006b053e3db43386be713c1e9063
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-04-25 16:03:09 -0400

    [CLASE 5] Relaciones en Bases de Datos con Laravel Factories y Seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Recipe;
+use App\Models\Tag;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create(['email' => 'd@mail.com']);
+        User::factory(29)->create();
+        
+        Category::factory(12)->create();
+        Recipe::factory(100)->create();
+        Tag::factory(40)->create();
+
+        // Many to many
+
+        $recipes = Recipe::all();
+        $tags = Tag::all();
+        foreach ($recipes as $recipe) {
+            $recipe->tags()->attach($tags->random(rand(2, 4)));
+        }
+    }
+}
