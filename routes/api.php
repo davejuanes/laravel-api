8a8e5949302d9c477049a6208c3f3aef3b647e81
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-04-29 14:17:04 -0400

    [CLASE 16] Creación de Rutas y Controladores para Iniciar Sesión en API

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
-use App\Http\Controllers\Api\CategoryController;
-use App\Http\Controllers\Api\RecipeController;
-use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\RecipeController;
+use App\Http\Controllers\Api\TagController;
+
+use App\Http\Controllers\Api\LoginController;
 /* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum'); */
+Route::get('login', [LoginController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('categories',                [CategoryController::class, 'index']);
